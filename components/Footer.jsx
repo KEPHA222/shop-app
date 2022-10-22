f4949ec4f93032698fd840c4862251df462d03e4
kepha222 <kephamotari1@.com>
2022-10-22 23:17:28 +0300

    initial commit

---
+++ components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>2022 © BestSella Headphones All rights reserved.</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+        <AiOutlineFacebook />
+      </p>
+    </div>
+  );
+};
+export default Footer;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import React from "react";
+import { Toast } from "react-hot-toast";
+
+import { Layout } from "../components";
+import "../styles/globals.css";
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
